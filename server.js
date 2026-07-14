de7f1f19247c929656257ab7adf7874e39997e71
ritesh suthar <riteshsuthar921@gmail.com>
2026-07-14 13:25:58 +0530

    naya hai bhai

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post('/xyz-api/chat', async (req, res) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                model: "llama3-8b-8192",
+                model: 'llama-3.1-8b-instant',
                 messages: messagesToSend // Frontend wala updated array bhej rahe hain
             })
         });
